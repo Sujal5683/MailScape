dd294f66cfa075d2cc1a1d492ee7a8305206b9ad
Sujal <sujal@mailscape.app>
2026-09-08 03:01:20 +0530

    DEPLOY

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   reactStrictMode: false,
   async rewrites() {
     return [
